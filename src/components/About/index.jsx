b3a702a0891b207b18cf067a6180e58f4f8ed701
Mileslegend <mileslegend7@gmail.com>
2024-06-13 15:05:57 +0000

    about done

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './About.css'
+import { whyChooseMe } from '../../sources'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about' >
+      <div className="wrapper">
+        <div className="section">
+          <h1 className="heading-1">
+            <span className="gradient-text">About Me</span>
+          </h1>
+          <h4 className="sub-title muted">
+          Armed with top-notch skills in UI/UX design, content creation, and full-stack development, I possesses the remarkable ability to create immersive digital journeys that leave a lasting impact. Whether it's crafting visually stunning interfaces, architecting seamless user experiences, or seamlessly blending front-end and back-end magic, My multidimensional skill set ensures the delivery of extraordinary online solutions that captivate and inspire.
+          </h4>
+        </div>
+        <div className="group">
+          {whyChooseMe.map((list, index)=> (
+            <div className="flex-center group-item" key={index}>
+              <div className="flex-center icon-wrapper">
+                {list.icon}
+              </div>
+              <h4 className="title">{list.title}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
